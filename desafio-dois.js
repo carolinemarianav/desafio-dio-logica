e6e52de6ad761b8cccc3d91d8ad02755980d72d3
Caroline Mariana Valerio <138807020+carolinemarianav@users.noreply.github.com>
2025-07-28 21:05:33 -0300

    Exercicio lógica

---
+++ desafio-dois.js
@@ -0,0 +1,27 @@
+function calcularRank(vitorias, derrotas) {
+    let saldo = vitorias - derrotas;
+    let nivel;
+
+    if (vitorias <= 10) {
+        nivel = "Ferro";
+    } else if (vitorias <= 20) {
+        nivel = "Bronze";
+    } else if (vitorias <= 50) {
+        nivel = "Prata";
+    } else if (vitorias <= 80) {
+        nivel = "Ouro";
+    } else if (vitorias <= 90) {
+        nivel = "Diamante";
+    } else if (vitorias <= 100) {
+        nivel = "Lendário";
+    } else {
+        nivel = "Imortal";
+    }
+
+    return `O herói tem saldo de ${saldo} e está no nível ${nivel}`;
+}
+
+let vitorias = 100;
+let derrotas = 10;
+let resultado = calcularRank(vitorias, derrotas);
+console.log(resultado);
